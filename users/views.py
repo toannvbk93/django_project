45f2117bf841d365f18c8b0ee6c67f3124bf9679
toannvbk93 <vantoanbk93@gmail.com>
2021-03-02 00:00:06 +0900

    Edit profile

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, ListView
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 
@@ -19,4 +19,26 @@ class RegisterView(TemplateView):
         return render(request, 'users/register.html', {'form': form})
 class ProfileView(TemplateView):
     def get(self, request, *args, **kwargs):
-        return render(request, 'users/profile.html')
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+        context = {
+            'u_form': u_form,
+            'p_form': p_form
+        }
+        return render(request, 'users/profile.html', context)
+    def post(self, request, *args, **kwargs):
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been update')
+            return redirect('/profile/')
+        context = {
+            'u_form': u_form,
+            'p_form': p_form
+        }
+
+        return render(request, 'users/profile.html', context)
+
+
