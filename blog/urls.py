974d60dfff66e5cfcb1e7f1370165becf28eee28
toannvbk93 <vantoanbk93@gmail.com>
2021-03-05 15:07:35 +0900

    create, detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from blog.views import HomeView, AboutView
+from blog.views import HomeView, AboutView, PostListView, PostDetailsView, PostCreateView
 
 
 app_name="blog"
 urlpatterns = [
-    path('', HomeView.as_view(), name="home"),
+    # path('', HomeView.as_view(), name="home"),
     # path('about/', AboutView.as_view(), name="about")
+    path('', PostListView.as_view(), name="home"),
+    path('post/<int:pk>/', PostDetailsView.as_view(), name="detail"),
+    path('post/new/', PostCreateView.as_view(), name="create"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
 from .models import Post
 # Create your views here.
 
@@ -11,3 +11,16 @@ class HomeView(TemplateView):
         return render(request, self.template_name, context)
 class AboutView(TemplateView):
     template_name = "about.html"
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+class PostDetailsView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'post_create.html'
