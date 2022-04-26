dfa36a4b97af8acd643d2687cf02a7c7f362117a
ToanNV <vantoanbk93@gmail.com>
2022-04-27 00:04:05 +0900

    fix test

--- blog/tests.py
+++ blog/tests.py
@@ -4,14 +4,14 @@ import datetime
 
 from django.utils import timezone
 # Create your tests here.
-from models import Post
+from blog.models import Post
 
 def create_post():
     return Post(title="Title content",content="Post content", date_posted=datetime.datetime.now())
 
 class HomeViewTests(TestCase):
     def test_post(self):
-        response = self.client.get(reverse('blog:index'))
+        response = self.client.get(reverse('blog:post-detail:pk'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Post content")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
